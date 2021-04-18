da02cdd54ffb585aa9d59f402e662dfd9310f87d
Fletman <lafletc@live.com>
2021-04-18 11:54:41 -0400

    all chat commands are promise-based to better handle metrics hooks

--- modules/database.js
+++ modules/database.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const { Client } = require('pg');
+const path_resolve = require('path').resolve;
+const logger = require('./fletlog.js');
 
 module.exports = {
-    get_db: async () => {
-        const client = new Client();
+    get_db: async (config_file = '../resources/db_config.json') => {
+        let db_config;
+        if(fs.existsSync(config_file)) {
+            logger.log(`Loading database config from file ${path_resolve(config_file)}`);
+            db_config = JSON.parse(fs.readFileSync(config_file));
+        } else {
+            logger.log("No database config file found. Using config defaults");
+            db_config = {};
+        }
+        const client = new Client(db_config);
         await client.connect();
         await init_db_schema(client);
         return client;
