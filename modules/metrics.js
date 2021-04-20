bcc32938d320eb30e2a12291c98ce4cd07e87d97
Fletman <lafletc@live.com>
2021-04-20 01:35:35 -0400

    compatibility fix: replace hostname dashes with underscores

--- modules/metrics.js
+++ modules/metrics.js
@@ -64,6 +64,10 @@ module.exports = class Fletrics {
     }
 }
 
+function get_host() {
+    return os.hostname().replace('-', '_');
+}
+
 async function console_log_cmd_metric(channel, command, start_time, latency, was_valid, caller) {
     const metric_obj = {
         channel: channel,
@@ -72,7 +76,7 @@ async function console_log_cmd_metric(channel, command, start_time, latency, was
         invoked: new Date(start_time).toLocaleString(),
         latency: `${latency}ms`,
         valid: was_valid,
-        host: os.hostname()
+        host: get_host()
     };
     this.datasource.log(metric_obj);
 }
@@ -82,7 +86,8 @@ async function console_log_pyramid_metric(channel, phrase, pyramid_time, user) {
         channel: channel,
         user: user,
         phrase: phrase,
-        time: pyramid_time.toLocaleString()
+        time: pyramid_time.toLocaleString(),
+        host: get_host()
     };
     this.datasource.log(metric_obj);
 }
@@ -91,7 +96,7 @@ async function insert_cmd_metric_to_pg(channel, command, start_time, latency, wa
     await this.datasource.query(
         `INSERT INTO fletbot.cmd_metric (channel, command, calling_user, invoke_time, valid, host, latency)
          VALUES ($1::text, $2::text, $3::text, $4::timestamp, $5::boolean, $6::text, $7::int)`,
-        [channel, command, caller, new Date(start_time).toISOString(), was_valid, os.hostname(), latency]
+        [channel, command, caller, new Date(start_time).toISOString(), was_valid, get_host(), latency]
     );
 }
 
@@ -99,6 +104,6 @@ async function insert_pyramid_metric_to_pg(channel, phrase, pyramid_time, user) 
     await this.datasource.query(
         `INSERT INTO fletbot.pyramid (channel, pyramid_user, phrase, pyramid_time, host)
          VALUES ($1::text, $2::text, $3::text, $4::timestamp, $5::text)`,
-        [channel, user, phrase, pyramid_time.toLocaleString(), os.hostname()]
+        [channel, user, phrase, pyramid_time.toLocaleString(), get_host()]
     );
 }
