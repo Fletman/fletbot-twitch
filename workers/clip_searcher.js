4b0f193fa0948af5e5641fa2def66552c779c423
Lance Fletcher <laf224@scarletmail.rutgers.edu>
2022-09-10 19:16:37 -0400

    advanced filters for fletclip

--- workers/clip_searcher.js
+++ workers/clip_searcher.js
@@ -2,15 +2,16 @@ const { parentPort } = require('worker_threads');
 const clip_searcher = require('../modules/clip_search.js');
 
 parentPort.on('message', (params) => {
-    if(params.clip_title === '*') {
-        clip_searcher.random_clip(params.client_id, params.token, params.channel)
+    console.log(params);
+    if(params.search_params.title === '*' || params.search_params.search_type === 'game') {
+        clip_searcher.random_clip(params.client_id, params.token, params.channel, params.search_params.game)
             .then((clip) => {
                 parentPort.postMessage(clip);
             }).catch((err) => {
                 throw (err);
             });
     } else {
-        clip_searcher.clip_search(params.client_id, params.token, params.channel, params.clip_title)
+        clip_searcher.clip_search(params.client_id, params.token, params.channel, params.search_params)
             .then((clip) => {
                 parentPort.postMessage(clip);
             }).catch((err) => {
